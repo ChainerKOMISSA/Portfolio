303733a63f1ca3c4e8179f7dea687f0ee0564159
Essi Chainer KOMISSA ZOTSU <essikomissa@gmail.com>
2024-08-27 22:41:41 +0200

    Initialisation du projet

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    </main>
+  );
+}
