a35486fb42a22b7295ba197bde352c62f2826a98
Essi Chainer KOMISSA ZOTSU <essikomissa@gmail.com>
2025-03-02 15:03:26 +0100

    Corrections3  apres merge

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -16,7 +16,6 @@ import { cn } from "@/lib/utils";
 
 
 import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { AnimatedTooltip } from "./AnimatedTooltip";
