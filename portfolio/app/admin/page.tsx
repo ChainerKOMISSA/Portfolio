4b99f1bf07b5b08a435194f741fb0317627b7c3b
Essi Chainer KOMISSA ZOTSU <essikomissa@gmail.com>
2025-05-21 20:05:34 +0200

    Composants AdminPage

--- portfolio/app/admin/page.tsx
+++ portfolio/app/admin/page.tsx
@@ -1,8 +1,17 @@
+import Navbar from "./ui/Navbar";
+import Footer from "./ui/Footer";
+import MainContent from "./ui/MainContent";
+import Sidebar from "./ui/Sidebar";
 
 export default function AdminPage() {
     return (
-        <>
-        </>
-
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <div className="flex flex-1">
+                <Sidebar />
+                <MainContent />
+            </div>
+            <Footer />
+        </div>
     );
 }
